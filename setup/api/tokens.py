85a251101a83659d1e63ebdb8040524dd352c757
charlesmendel70 <charlesmendel70@gmail.com>
2022-01-19 17:05:26 +0200

    Added user data to tokens

--- setup/api/tokens.py
+++ setup/api/tokens.py
@@ -2,15 +2,19 @@ from flask import jsonify, request
 from setup import db
 from setup.api import bp
 from setup.api.auth import verify_password, token_auth
+from setup.api.errors import error_response
 
 @bp.route('/tokens', methods=['POST'])
 def get_token():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     user = verify_password(username, password)
-    token = user.get_token()
+    if user is None:
+        return error_response(401, "Incorrect username or password")
+    data = user.to_dict()
+    data['token'] = user.get_token()
     db.session.commit()
-    return jsonify({'token': token})
+    return jsonify(data)
 
 @bp.route('/tokens', methods=['DELETE'])
 @token_auth.login_required
