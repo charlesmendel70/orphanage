c77ef96037285617286d48e66c25a9261156e013
charlesmendel70 <charlesmendel70@gmail.com>
2022-01-30 13:02:40 +0200

    Changed app to application

--- orph.py
+++ orph.py
@@ -1,9 +1,9 @@
 from setup import create_app, db
 from setup.models import User, Orphanage, Message, Donation
 
-app = create_app()
+application = create_app()
 
 # the decorator below registers the function as a shell context function
-@app.shell_context_processor 
+@application.shell_context_processor 
 def make_shell_context():
     return {'db': db,'User': User, 'Orphanage': Orphanage, 'Message': Message, 'Donation': Donation}
